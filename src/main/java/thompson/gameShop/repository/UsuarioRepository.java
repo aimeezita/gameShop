8a3e0bb951574f233664a58fc323ad513491cd26
aimeezita <rosettethompson@gmail.com>
2022-08-19 15:04:47 -0300

    implementação da security

---
+++ src/main/java/thompson/gameShop/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package thompson.gameShop.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import thompson.gameShop.model.Usuario;
+
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Optional<Usuario> findByUsuario(String usuario);
+}
